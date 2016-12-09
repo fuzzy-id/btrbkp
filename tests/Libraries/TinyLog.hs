467d1997380ca89592272bd82ae6024fa9e910b8
Thomas Bach <t.bach@ilexius.de>
2016-12-09 13:10:28 +0100

    cleaning up

--- tests/Libraries/TinyLog.hs
+++ tests/Libraries/TinyLog.hs
@@ -17,14 +17,16 @@ tests = $(testGroupGenerator)
 
 case_log_to_file_and_read_back = 
     ((unlines $ zipWith (\l n -> l:", " ++ show n) "TDIWEF" [0..]) @=?) =<< contentOfLoggerOnTempFile s f
-  where f logger = mapM_ (uncurry (log logger)) [ (Trace, msg "0")
-                                                , (Debug, msg "1")
-                                                , (Info,  msg "2")
-                                                , (Warn,  msg "3")
-                                                , (Error, msg "4")
-                                                , (Fatal, msg "5")
-                                                ]
-        s = (setLogLevel Trace . setFormat Nothing) defSettings
+  where
+    s = (setLogLevel Trace . setFormat Nothing) defSettings
+    f logger = mapM_ (uncurry (log logger)) [ (Trace, msg "0")
+                                            , (Debug, msg "1")
+                                            , (Info,  msg "2")
+                                            , (Warn,  msg "3")
+                                            , (Error, msg "4")
+                                            , (Fatal, msg "5")
+                                            ]
 
 case_level_Trace_not_logged_by_default = contentOfLoggerOnTempFile defSettings f >>= (@?= "")
-  where f logger = log logger Trace (msg "foo")
+  where
+    f logger = log logger Trace (msg "foo")
