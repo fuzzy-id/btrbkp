557b6965dcb9067923ad12aa63747ddf69dc037f
Thomas Bach <t.bach@ilexius.de>
2016-12-23 21:39:31 +0100

    setup test volume

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # All checks happen in the test main functions…
 
+set -e
+
 # This is how you can create an appropriate testing volume:
 # $ dd if=/dev/zero of=/tmp/test_dev bs=1M count=100
 # $ mkfs.btrfs /tmp/test_dev
@@ -9,11 +11,28 @@
 # # chown USER.USER /mnt/tests
 # $ run_tests.sh /mnt/tests
 
-if [ ${#} != 1 ]; then
-    echo "Usage: ${0} VOL"
+if [[ ${#} == 0 || ${#} > 2 ]]; then
+    echo "Usage: ${0} [setup|VOL]"
     echo
-    echo "Runs the tests passing VOL as directory"
+    echo "If 'setup' is the only argument then a temporary file is"
+    echo "created, formated as btrfs and mounted on a temporary"
+    echo "directory."
+    echo
+    echo "Otherwise runs the tests passing VOL as directory"
     echo "to run btrfs tests in."
+    exit 1
+elif [[ "${1}" == "setup" ]]; then
+    DEV="$(mktemp --tmpdir btrbkp-test-dev-XXX)"
+    MNT="$(mktemp --tmpdir --directory btrbkp-test-mnt-XXX)"
+    VOL="${MNT}/tests"
+    dd if=/dev/zero of="${DEV}" bs=1M count=100
+    mkfs.btrfs "${DEV}"
+    sudo mount -t btrfs -o user_subvol_rm_allowed "${DEV}" "${MNT}"
+    sudo btrfs subvolume create "${VOL}"
+    sudo chown ${USER}.${USER} "${VOL}"
 else
-    cabal test --show-details=always --test-option=--test-vol --test-option="${1}"
+    cabal test \
+          --show-details=always \
+          --test-option=--test-vol \
+          --test-option="${1}"
 fi
