028d20d841f1ebac8e5ade3660621b5b189351e9
Thomas Bach <t.bach@ilexius.de>
2017-02-07 16:36:16 +0100

    first working version

--- src/Btrbkp.hs
+++ src/Btrbkp.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Btrbkp where
 
-import qualified Data.Text as T
 import Control.Lens ((^.), makeLenses, view)
 import Control.Monad.Except (ExceptT(), runExceptT)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Reader (ReaderT(), runReaderT)
+import Data.Hourglass (DateTime(), timePrint)
+import Data.Maybe (isNothing)
+import qualified Data.Text as T
+import Data.Traversable (forM)
 import System.Linux.Btrfs (snapshot)
+import System.FilePath ((</>), pathSeparator, splitDirectories)
 import qualified System.Logger as L
 import Time.System (dateCurrent)
 
 import Btrbkp.Config
+import Util.Findmnt (findmnt)
 
 data BtrbkpEnv = BtrbkpEnv { _getLogger :: L.Logger
+                           , _getDateTime :: DateTime
                            }
 
 makeLenses ''BtrbkpEnv
@@ -29,8 +35,10 @@ runBtrbkp env bkp = do res <- (runReaderT . runExceptT) bkp env
                          Left e   -> (return . Just) e
 
 btrbkp :: MonadIO m => FilePath -> FilePath -> BtrbkpT m
-btrbkp src dest = do bkpLog L.Info (src `msgTo` dest)
-                     liftIO (snapshot dest src True)
+btrbkp src dest = do timeStamp <- timePrint "YYYY-MM-DD_H-MI-S" <$> view getDateTime
+                     let dest' = dest </> timeStamp ++ ":" ++ (last . splitDirectories) src
+                     bkpLog L.Info (src `msgTo` dest')
+                     liftIO (snapshot src dest' True)
                      bkpLog L.Info (L.msg "Done")
 
 msgTo :: String -> String -> L.Msg -> L.Msg
@@ -40,21 +48,42 @@ bkpLog :: MonadIO m => L.Level -> (L.Msg -> L.Msg) -> BtrbkpT m
 bkpLog lvl msg = do l <- view getLogger
                     liftIO (L.log l lvl msg)
 
-runBkp :: MonadIO m => BtrbkpConfig -> m ()
+runBkp :: MonadIO m => BtrbkpConfig -> m (Maybe BkpException)
 runBkp cfg = do env <- createEnv cfg
                 bMap <- mkBkpMap (cfg ^. destinations) (cfg ^. sources)
-                undefined
+                unfoldBkp env bMap
+  where
+    unfoldBkp :: MonadIO m => BtrbkpEnv -> [BtrbkpT m] -> m (Maybe BkpException)
+    unfoldBkp env []     = do finalize env
+                              return Nothing
+    unfoldBkp env (b:bs) = do res <- runBtrbkp env b
+                              if isNothing res
+                                 then unfoldBkp env bs
+                                 else do finalize env
+                                         return res
+    finalize env = do L.flush (env ^. getLogger)
+                      L.close (env ^. getLogger)
 
 createEnv :: MonadIO m => BtrbkpConfig -> m BtrbkpEnv
 createEnv cfg = BtrbkpEnv <$> createLogger cfg
+                          <*> liftIO dateCurrent
 
 createLogger :: MonadIO m => BtrbkpConfig -> m L.Logger
 createLogger _ = (liftIO . L.create) L.StdOut
 
 mkBkpMap :: MonadIO m => [BtrbkpDestination] -> [BtrbkpSource] -> m [BtrbkpT m]
-mkBkpMap [] _ = return []
+mkBkpMap []        _        = return []
+mkBkpMap _         []       = return []
 mkBkpMap (d:dests) (s:srcs) = do bkp <- analyze d s
-                                 thisDest <- (bkp:) <$> mkBkpMap dests (s:srcs)
-                                 (thisDest `mappend`) <$> mkBkpMap dests srcs
+                                 thisDest <- (bkp:) <$> mkBkpMap (d:dests) srcs
+                                 (thisDest `mappend`) <$> mkBkpMap dests (s:srcs)
 
-analyze (BtrbkpDestination d) (BtrbkpSource s) = return $ btrbkp (T.unpack s) (T.unpack d)
+analyze :: MonadIO m => BtrbkpDestination -> BtrbkpSource -> m (BtrbkpT m)
+analyze (BtrbkpDestination d) (BtrbkpSource s) = do srcUuid <- findmnt src
+                                                    tgtUuid <- findmnt dest
+                                                    if srcUuid == tgtUuid
+                                                       then return $ btrbkp src dest
+                                                       else undefined
+                                                    
+  where
+    (src, dest) = (T.unpack s, T.unpack d)
--- src/Exec/Btrbkp.hs
+++ src/Exec/Btrbkp.hs
@@ -1,20 +1,23 @@
 module Exec.Btrbkp (main) where
 
 import Data.Ini (readIniFile)
+import Data.Maybe (maybe)
 import Options.Applicative
+import System.Exit (ExitCode(ExitSuccess), die, exitWith)
 
+import Btrbkp
 import Btrbkp.Config
 
 data Opts = Opts { configFile :: FilePath
                  }
                  deriving (Eq,Show)
 
-
 main :: IO ()
 main = do opts <- execParser optsDesc
           ini <- errorOnLeft <$> (readIniFile . configFile) opts
           let cfg = errorOnLeft $ configFromIni ini
-          undefined
+          maybeExc <- runBkp cfg
+          maybe (exitWith ExitSuccess) (die . show) maybeExc
   where
     optsDesc = info (helper <*> optsParser) (fullDesc <> progDesc "Backup btrfs volumes.")
     optsParser = Opts <$> strOption (short 'c' 
